10e5545da48dc96aa551fc08e87bfe6ff9cf7917
Matej Vasek <mvasek@redhat.com>
2021-07-07 19:18:24 +0200

    fixup
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -17,6 +19,23 @@ func main() {
 
 	decoder := json.NewDecoder(os.Stdin)
 	decoder.Decode(&data)
+
+	script := `cd $(mktemp -d)
+git clone https://github.com/boson-project/func/
+cd func
+go install ./cmd/func
+cd ..
+rm -fr func
+`
+	cmd := exec.Command("/bin/bash")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = strings.NewReader(script)
+
+	err := cmd.Run()
+	if err != nil {
+		os.Exit(1)
+	}
 
 	fmt.Printf("data: %+v\n", data)
 }
