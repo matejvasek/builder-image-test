238da3e2de597bf16938cc96e29e14f8645cba60
Matej Vasek <mvasek@redhat.com>
2021-07-07 17:07:51 +0200

    Parse input
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Getenv("GITHUB_EVENT"))
+	data := struct {
+		DockerUrl   string `json:"docker_url"`
+		Repository  string `json:"repository"`
+		UpdatedTags string `json:"updated_tags"`
+		Namespace   string `json:"namespace"`
+		Name        string `json:"name"`
+	}{}
+
+	decoder := json.NewDecoder(os.Stdin)
+	decoder.Decode(&data)
+
+	fmt.Printf("data: %+v\n", data)
 }
